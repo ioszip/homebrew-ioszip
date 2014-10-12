1171bc5db440a7ea59d95e3e5ec5c5bafb59b7fa
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2014-10-12 18:28:23 +0900

    modify

--- ioszip.rb
+++ ioszip.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class IoszipGithubIo < Formula
+class Ioszip < Formula
   homepage "https://github.com/ioszip/ioszip.github.io"
   url "https://github.com/ioszip/ioszip.github.io/archive/1.0.0.zip"
   sha1 "114122a1ce3f71dc5cff6d2c1be27a15b40a5da2"
